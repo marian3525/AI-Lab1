98b2a781302014b5d1339512f8673b41699df6b6
marian <marian3525@yahoo.com>
2019-03-10 19:26:01 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/lab1_C__.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/marian/Documents/CS/AI/lab1_C++/main.cpp" "/home/marian/Documents/CS/AI/lab1_C++/cmake-build-debug/CMakeFiles/lab1_C__.dir/main.cpp.o"
+  "/home/marian/Documents/CS/AI/lab1_C++/sources/Controller.cpp" "/home/marian/Documents/CS/AI/lab1_C++/cmake-build-debug/CMakeFiles/lab1_C__.dir/sources/Controller.cpp.o"
+  "/home/marian/Documents/CS/AI/lab1_C++/sources/Problem.cpp" "/home/marian/Documents/CS/AI/lab1_C++/cmake-build-debug/CMakeFiles/lab1_C__.dir/sources/Problem.cpp.o"
+  "/home/marian/Documents/CS/AI/lab1_C++/sources/State.cpp" "/home/marian/Documents/CS/AI/lab1_C++/cmake-build-debug/CMakeFiles/lab1_C__.dir/sources/State.cpp.o"
+  "/home/marian/Documents/CS/AI/lab1_C++/sources/UI.cpp" "/home/marian/Documents/CS/AI/lab1_C++/cmake-build-debug/CMakeFiles/lab1_C__.dir/sources/UI.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab1_C__.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1_C__.dir/main.cpp.o"
+  "CMakeFiles/lab1_C__.dir/sources/Controller.cpp.o"
+  "CMakeFiles/lab1_C__.dir/sources/Problem.cpp.o"
+  "CMakeFiles/lab1_C__.dir/sources/State.cpp.o"
+  "CMakeFiles/lab1_C__.dir/sources/UI.cpp.o"
+  "lab1_C__.pdb"
+  "lab1_C__"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
